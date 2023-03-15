a9194d1f3761a7aafc8a7532628675ed00a3d9aa
SamaMansour <sma302000@gmail.com>
2023-03-15 23:48:14 +0300

    Add icons

--- src/stories/Card.tsx
+++ src/stories/Card.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './card.css';
-import { Card, CardHeader, CardBody, CardFooter, Heading, Text, Stack, Box, StackDivider, Radio, RadioGroup, Divider, Link, Icon, Center} from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Heading, Text, Stack, Box, StackDivider, Radio, RadioGroup, Divider, Link, Icon, Center, Button, ButtonGroup, CreateIcon} from '@chakra-ui/react';
+import { createIcon } from '@chakra-ui/icons';
+import { MdStarRate, MdEdit } from 'react-icons/md'
+
 /* eslint-disable */
 interface CardProps {
   
@@ -13,6 +16,16 @@ interface CardProps {
   onClick?: () => void;
 }
 
+export const UpDownIcon = createIcon({
+  displayName: 'UpDownIcon',
+  viewBox: '0 0 200 200',
+  path: (
+    <path
+      fill='red'
+      d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
+    />
+  ),
+})
 export const ItemCard = ({
   size = 'medium',
   language = 'right',
@@ -37,12 +50,20 @@ export const ItemCard = ({
   <Card w ='100%' border='1px' borderColor= {border}>
   <CardHeader>
   <RadioGroup  onChange={controlBorder} value={buttonValue}>
-  <Stack>
+  <Stack spacing={16} direction='row'>
     <Radio value='1' onClick = {()=> setBorder('grey')}>
     <Heading size='md' textAlign = {language}>{label}</Heading>
     </Radio>
+    <ButtonGroup spacing='6'>
+      <Button flex='1' colorScheme='whiteAlpha' leftIcon={ <Icon as={MdStarRate} w={8} h={8} color='blue.500' />}>
+      </Button>
+      <Button flex='1'colorScheme='whiteAlpha' leftIcon={ <Icon as={MdEdit} w={8} h={8} color='blue.500' />}>
+      </Button>
+</ButtonGroup>
   </Stack>
 </RadioGroup>
+
+
 </CardHeader>
   <Center>
   <Divider width = '80%' />
